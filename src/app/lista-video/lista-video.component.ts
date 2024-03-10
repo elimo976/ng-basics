30b9c41b81d4d19d4d3692d8a66f5852503da1c1
elimo976 <161508542+elimo976@users.noreply.github.com>
2024-03-10 19:03:25 +0100

    Add files via upload

--- src/app/lista-video/lista-video.component.ts
+++ src/app/lista-video/lista-video.component.ts
@@ -29,3 +29,20 @@ export class ListaVideoComponent {
     this.safeUrls = this.videos.map(video => this.sanitizer.bypassSecurityTrustResourceUrl(video.url));
   }
 }
+
+/*
+SPIEGAZIONE PER "SANIFICARE" L'URL PER RENDERLO SICURO PER ANGULAR:
+
+- Dichiarazione della variabile: Il codice dichiara un array di SafeResourceUrl chiamato safeUrls. SafeResourceUrl è un tipo (sì, proprio tipo) fornito da Angular che rappresenta un URL sicuro che può essere utilizzato in contesti in cui è richiesta una certa sicurezza, come ad esempio quando si utilizzano URL provenienti da sorgenti non attendibili. In pratica il SafeResourceUrl marca gli URL come sicuri.
+
+- Costruttore: Il costruttore della classe accetta un'istanza di DomSanitizer. Questo servizio è fornito da Angular per manipolare i valori di DOM in modo sicuro. Viene iniettato tramite dependency injection (La dependency injection (DI) è un design pattern che serve per gestire le dipendenze tra i diversi componenti di un'applicazione in modo più modulare, flessibile e facilmente testabile).
+
+- Inizializzazione di safeUrls: All'interno del costruttore, c'è una riga di codice che inizializza l'array safeUrls utilizzando il metodo map() su l'array 'videos'.
+
+- map() viene utilizzato per iterare su ogni elemento dell'array videos.
+Per ogni video, sanitizer.bypassSecurityTrustResourceUrl() viene chiamato sull'URL del video. Questo metodo fa parte del DomSanitizer e ritorna un oggetto SafeResourceUrl, che rappresenta l'URL sicuro.
+L'array risultante di SafeResourceUrl viene assegnato a safeUrls.
+
+- Il metodo bypassSecurityTrustResourceUrl() esiste per accettare un URL come parametro restituendo un oggetto SafeResourceUrl. Questo oggetto rappresenta l'URL marcato come sicuro dal sistema di sanitizzazione di Angular. In pratica, ciò significa che Angular è consapevole che l'URL fornito è considerato sicuro e può essere utilizzato in modo sicuro all'interno dell'applicazione.
+
+*/
